7fa74538c1f5b07cc30e22ff88073ff090301cf8
Bartlomiej Zalas <bartekzalas@gmail.com>
2015-10-21 11:32:46 +0200

    id -> item_id and timazone fix

--- create.sql
+++ create.sql
@@ -1,6 +1,6 @@
 
 create table users(
-  id bigserial primary key,
+  user_id bigserial primary key,
   email character varying(255) not null unique,
   password character varying(255) not null,
   latitude real,
@@ -11,7 +11,7 @@ create table users(
 
 
 create table places(
-  id bigserial primary key,
+  place_id bigserial primary key,
   name character varying(127) not null,
   latitude real not null,
   longitude real not null,
@@ -20,34 +20,34 @@ create table places(
 
 
 create table events(
-  id bigserial primary key,
+  event_id bigserial primary key,
   name character varying(127) not null,
   description character varying(511),
-  created_at timestamp without timezone not null default now(),
-  starts_at timestamp without timezone not null,
-  ends_at timestamp without timezone not null,
-  user_id bigint not null references users(id) ,
-  place_id bigint not null references places(id)
+  created_at timestamp without time zone not null default now(),
+  starts_at timestamp without time zone not null,
+  ends_at timestamp without time zone not null,
+  user_id bigint not null references users(user_id) ,
+  place_id bigint not null references places(place_id)
 );
 
 
 create table participants(
-  user_id bigint not null references users(id) on delete cascade,
-  event_id bigint not null references events(id) on delete cascade,
+  user_id bigint not null references users(user_id) on delete cascade,
+  event_id bigint not null references events(event_id) on delete cascade,
   primary key(user_id, event_id)
 );
 
 
 create table ratings(
-  user_id bigint not null references users(id) on delete cascade,
-  event_id bigint not null references events(id) on delete cascade,
+  user_id bigint not null references users(user_id) on delete cascade,
+  event_id bigint not null references events(event_id) on delete cascade,
   value smallint not null,
   primary key(user_id, event_id)
 );
 
 create table tickets(
-  id bigint primary key,
-  event_id bigint not null REFERENCES events(id) on delete cascade,
+  ticket_id bigint primary key,
+  event_id bigint not null REFERENCES events(event_id) on delete cascade,
   sold_amount int not null default 0,
   max_amount int,
   price real not null

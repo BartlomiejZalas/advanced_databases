1106a5bb290ce801a88f7d12deeee79aa593840a
Bartlomiej Zalas <bartekzalas@gmail.com>
2015-10-28 12:37:49 +0100

    redundent semicolon removed

--- db - report2.sql
+++ db - report2.sql
@@ -49,7 +49,7 @@ $do$
 BEGIN
 IF exists (SELECT 1 FROM TICKETS WHERE event_id = 10 AND sold_amount < max_amount) THEN
 	INSERT INTO participants VALUES(1, 3);
-	UPDATE tickets SET sold_amount = sold_amount+1 WHERE event_id = 10;;
+	UPDATE tickets SET sold_amount = sold_amount+1 WHERE event_id = 10;
 	UPDATE users SET last_activity = now() WHERE user_id = 1;
 END IF;
 END

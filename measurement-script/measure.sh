e4026fb9af1c6224fad24c4477158ef6cb6281be
Bartlomiej Zalas <bartekzalas@gmail.com>
2015-10-30 21:49:49 +0100

    script - working

--- measurement-script/measure.sh
+++ measurement-script/measure.sh
@@ -9,15 +9,16 @@ function cleanResults {
 }
 
 function restoreDB {
-	echo "CLEANING AND RESTORING DATABAS";
+	echo "CLEANING AND RESTORING DATABASE";
 	psql -h localhost -d database1 -U postgres < "$MAIN_DIRECTORY/data/beforeImport.sql";
 	pg_restore -i -h localhost -p 5432 -U postgres -d database1 -v "$MAIN_DIRECTORY/data/$BACKUP";
 }
 
 function executeSQL {
 	echo "EXECUTING QUERY: $1"
-	time=$(psql -h localhost -d database1 -U postgres < "$MAIN_DIRECTORY/measurement-script/transactions/$1" | grep "Time:" | tail -1);
-	echo -e "$1 $time" >> $FILE;
+	time=$(psql -h localhost -d database1 -U postgres < "transactions/$1" | grep "Time:" | awk '{s+=$2} END {printf "%.0f", s}' );
+	timeInSeconds=$(awk -v time=$time 'BEGIN { print (time / 1000) }');	
+	echo -e "$1 $timeInSeconds seconds" >> $FILE;
 }
 
 cleanResults;
@@ -29,6 +30,12 @@ restoreDB;
 executeSQL "addParticipant.sql";
 
 restoreDB;
+executeSQL "topProfitableEvents.sql";
+
+restoreDB;
+executeSQL "nearestPlaces.sql";
+
+restoreDB;
 executeSQL "addParticipantMostPopularEvents.sql";
 
 restoreDB;
@@ -36,11 +43,5 @@ executeSQL "deleteSpam.sql";
 
 restoreDB;
 executeSQL "moveToReplacementLocation.sql";
-
-restoreDB;
-executeSQL "nearestPlaces.sql";
-
-restoreDB;
-executeSQL "topProfitableEvents.sql";
 
 echo "Time measurements ready. Check $FILE for more details."

bc6b669a4b90779bc9ba10132cea40a284272f11
Bartlomiej Zalas <bartekzalas@gmail.com>
2015-10-30 22:05:06 +0100

    script - conf parameters

--- measurement-script/measure.sh
+++ measurement-script/measure.sh
@@ -3,6 +3,8 @@
 FILE=resuls.txt
 BACKUP=backup10k.backup
 MAIN_DIRECTORY="/home/bartek/advancedDatabases"
+PG_USER=postgres
+PG_DB_NAME=database1
 
 function cleanResults {
 	rm -f $FILE;
@@ -10,13 +12,13 @@ function cleanResults {
 
 function restoreDB {
 	echo "CLEANING AND RESTORING DATABASE";
-	psql -h localhost -d database1 -U postgres < "$MAIN_DIRECTORY/data/beforeImport.sql";
-	pg_restore -i -h localhost -p 5432 -U postgres -d database1 -v "$MAIN_DIRECTORY/data/$BACKUP";
+	psql -h localhost -d $PG_DB_NAME -U $PG_USER < "$MAIN_DIRECTORY/data/beforeImport.sql";
+	pg_restore -i -h localhost -p 5432 -U $PG_USER -d $PG_DB_NAME -v "$MAIN_DIRECTORY/data/$BACKUP";
 }
 
 function executeSQL {
 	echo "EXECUTING QUERY: $1"
-	time=$(psql -h localhost -d database1 -U postgres < "transactions/$1" | grep "Time:" | awk '{s+=$2} END {printf "%.0f", s}' );
+	time=$(psql -h localhost -d $PG_DB_NAME -U $PG_USER < "$MAIN_DIRECTORY/measurement-script/transactions/$1" | grep "Time:" | awk '{s+=$2} END {printf "%.0f", s}' );
 	timeInSeconds=$(awk -v time=$time 'BEGIN { print (time / 1000) }');	
 	echo -e "$1 $timeInSeconds seconds" >> $FILE;
 }
